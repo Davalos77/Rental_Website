454eec0319f343431554ce045ba83b7f892cfb77
Davalos77 <dennisavalos95@gmail.com>
2024-04-21 21:37:13 -0500

    creating navbar

---
+++ app/properties/add/page.jsx
@@ -0,0 +1,5 @@
+const PropertyAddPages = () => {
+  return <div>PropertyAddPages</div>;
+};
+
+export default PropertyAddPages;
---
+++ app/properties/page.jsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+const PropertiesPages = () => {
+  return (
+    <div>
+      <h1 className="text-3xl">Properties</h1>
+      <Link href="/properties">Go Home</Link>
+    </div>
+  );
+};
+
+export default PropertiesPages;
